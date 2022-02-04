112f5ec6814f8f86294e64da15248c97effcfe76
Zkrallah <muhammad.heshamyt@gmail.com>
2022-02-05 00:38:42 +0200

    LinkedListV1

--- src/App.java
+++ src/App.java
@@ -2,20 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        // declare a new Array Object and pass an initial index of 3 items
-        Array numbers = new Array(3);
-
-        // insert some integers 
-        numbers.insert(6);
-        numbers.insert(5);
-        numbers.insert(3);
-
-        // here we add numbers out of the index of the array but it's ok, it's handeld
-        numbers.insert(2);
-        numbers.insert(11);
-
-        //choose the methods you want to test here :
-        // get the data from the user
-        
+        // test the structures here
+    
     }
 }
